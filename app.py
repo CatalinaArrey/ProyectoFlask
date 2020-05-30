6d5f44cfef43f3532ad03d2f054a94c9838d7803
Cata Arrey <catalina.arrey@usm.cl>
2020-05-30 17:12:56 -0400

    primera version de la app

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("Wena los k")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Ayuda no entiendo nada"
+
+if __name__ == "__main__":
+    app.run()
